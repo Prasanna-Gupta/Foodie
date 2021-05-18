33e5420d9c060c2af652bf03e9a112d381ed7568
Prasanna <guptaprasanna171@gmail.com>
2021-05-18 22:00:45 +0530

    Added expand and contract search box

--- foodies.js
+++ foodies.js
@@ -1,3 +1,12 @@
 function searchBoxShow(){
-    
+    var x = document.getElementById("search-box");
+    var y = document.getElementById("search");
+    if (x.style.display === "none"){
+        x.style.display = "flex";
+        y.style.backgroundColor = "white";
+    }
+    else{
+        x.style.display = "none";
+        y.style.backgroundColor = "transparent";
+    }
 }
